0cfe1250d9c67f170c39fb2641ea722e865211be
TerraHub <info@terrhub.org>
2013-07-21 16:46:43 -0500

    added many-to-many model into the admin

--- ga_applications/admin.py
+++ ga_applications/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from mezzanine.pages import admin as pages_admin
+#from mezzanine.pages import admin as pages_admin
 from .models import *
 
-#class LayersInline(admin.TabularInline):
-#    model = ApplicationLayer
-#    extra = 1
+class LayersInline(admin.TabularInline):
+    model = ApplicationLayer
+    extra = 1
 
-#class ApplicationAdmin(pages_admin.PageAdmin):
-#    inlines = (LayersInline,)
+class ApplicationAdmin(admin.ModelAdmin):
+    inlines = (LayersInline,)
 
-admin.site.register(Application, admin.ModelAdmin)
-admin.site.register(ApplicationLayer, admin.ModelAdmin)
+admin.site.register(Application, ApplicationAdmin)
+#admin.site.register(ApplicationLayer, admin.ModelAdmin)
