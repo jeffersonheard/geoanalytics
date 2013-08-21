554daf3f9caa8ed55a0b4a0b87a723dd421d265e
jefferson heard <jeff@renci.org>
2013-08-21 11:45:09 -0400

    updated installer

--- requirements/install.sh
+++ requirements/install.sh
@@ -6,6 +6,6 @@ pip install Cython
 pip install numexpr
 pip install tables
 export CFLAGS=-I/usr/include/gdal
-pip install GDAL=1.8.1
+pip install GDAL==1.8.1
 export CFLAGS=
 pip install -r ga.renci.org.txt
