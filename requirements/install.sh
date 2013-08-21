5cbd4ef0a700f2de2982f4ee20f61feb4cedc765
Jeff Heard <jeff@ga-hydroshare-dev.renci.org>
2013-08-21 09:24:21 -0400

    updated requirements

--- requirements/install.sh
+++ requirements/install.sh
@@ -5,4 +5,7 @@ pip install scipy
 pip install Cython
 pip install numexpr
 pip install tables
+export CFLAGS=-I/usr/include/gdal
+pip install GDAL=1.8.1
+export CFLAGS=
 pip install -r ga.renci.org.txt
